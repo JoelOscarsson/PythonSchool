e44776783fe3e99e88bd7bf772e6b12ebdc4658d
JoelOscarsson <j_oscarsson@hotmail.com>
2022-10-03 15:04:03 +0200

    test_vector

--- Code-Along/L14-Unit-Testing/tests/test_vector.py
+++ Code-Along/L14-Unit-Testing/tests/test_vector.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import sys, os  #Man kan importera flera saker samtidigt
 import unittest
 
@@ -16,7 +17,20 @@ print(path_to_vector_module)
 from vector import Vector
 
 class TestVector(unittest.TestCase):
+    # Om vi ska använda flera vectorer så kan man använda en form utav setup
+    # setUp gives attributes that we can use later on
+    # this is to avoid repeating too much
+    def setUp(self):
+        self.x, self.y = 1, 2
+
+    # Can be used to create default 2D vector for testing
+    def create_2D_vector(self) -> Vector:
+        #v = Vector(1,2)
+        self.assertEqual(v.numbers, (self.x, self.y))
+        return Vector(self.x, self.y)
+
     def test_create_2D_vector(self):
+        """Testing numbers property"""
         v = Vector(1,2)
         self.assertEqual(v.numbers, (1,2))
 
@@ -24,6 +38,46 @@ class TestVector(unittest.TestCase):
         v = Vector(1,2,3)
         self.assertEqual(v.numbers, (1,2,3))
 
+    def test_create_empty_vector(self):
+        # If we get a raised ValueError -> the test is OK
+        with self.assertRaises(ValueError):
+            v = Vector()
+
+
+    def test_equal_2D_vectors(self):
+        v1 = self.create_2D_vector()
+        v2 = Vector(1,2)
+        self.assertEqual(v1, v2)
+
+    def test__not_equal_2D_vectors(self):
+        v1 = self.create_2D_vector()
+        v2 = Vector(-1, -2)
+        self.assertNotEqual(v1, v2)
+
+    def test_add_2_vectors(self):
+        v1 = self.create_2D_vector()
+        v2 = Vector(1,3)
+        self.assertEqual(v1+v2, Vector(self.x+1, self.y+3))
+
+    def test_getitem(self):
+        v1 = self.create_2D_vector()
+
+        # this approach is tedious when we have many elements in vector
+        #self.assertEqual(v1[0], self.x)
+        #self.assertEqual(v1[1], self.y)
+
+        # i = 0, number = 1 
+        # i = 1, number = 2
+        for i, number in enumerate((1,2)):
+            self.assertEqual(v1[i], number)
+
+    #TODO: Many more tests to be performed
+    
+
+class TestVector2(unittest.TestCase):
+    def test_create_2D_vector(self):
+        v = Vector(1, 2)
+        self.assertEqual(v.numbers, (1, 2))
 
 if __name__ == "__main__":
     unittest.main()
