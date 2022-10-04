90ba61595d3d6ba24946137ca0dc98af9aa3d358
JoelOscarsson <j_oscarsson@hotmail.com>
2022-10-04 13:09:57 +0200

    Shape

---
+++ Labb-3/Shape/Shape.py
@@ -0,0 +1,7 @@
+#TODO Skapa en parent klass shape
+#TODO: Skapa x y 
+#TODO Lägg in property för att utvidja funktioner och skrivskydda grundkoden
+#TODO 
+#TODO Lägg in koordinaterna som en int
+
+import matplotlib.pyplot as plt
