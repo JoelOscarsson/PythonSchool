536b96e7861b26e4eb38b0b63f50160bcb349011
JoelOscarsson <j_oscarsson@hotmail.com>
2022-10-12 17:02:11 +0200

    updated circle and geometry

--- Labb-3/Shape/circle.py
+++ Labb-3/Shape/circle.py
@@ -1,4 +1,258 @@
-from __future__ import annotations
+#from __future__ import annotations
 # Imports from geometry.py and and than then Shapes module
-from geometry import Shapes
-from math import pi
+#from geometry import Shapes
+#from math import pi
+
+
+from __future__ import annotations
+from copy import copy
+
+class GeomShape:
+    """Main class defining center point (x, y) of shape"""
+    def __init__(self, x_center: float|int , y_center: float|int) -> None:
+        #print("GS__init__ körs")
+        self.x_center = x_center
+        self.y_center = y_center
+    
+    # getters
+    @property
+    def x_center(self) -> float|int:
+        #print("GS x_center getter körs")
+        return self._x_center
+    
+    @property
+    def y_center(self) -> float|int:
+        #print("GS y_center getter körs")
+        return self._y_center
+
+    #setters
+    @x_center.setter
+    def x_center(self, value: float|int) -> float|int:
+        #print("GS x_center setter körs")
+        if not isinstance(value, (float, int)):
+            raise TypeError (f"x_center must be an int or float and not {type(value)}")
+        self._x_center = value
+    
+    @y_center.setter
+    def y_center(self, value: float|int) -> float|int:
+        #print("GS y_center setter körs")
+        if not isinstance(value, (float, int)):
+            raise TypeError (f"y_center must be an int or float and not {type(value)}")
+        self._y_center = value
+    
+    def __repr__(self) -> str:
+        return (f"Superclass 'GeomShapes', '{self.x_center}', '{self.y_center}'")
+    
+    def __str__(self) -> str:
+        return (f"Shape centered in (x,y) = ({self.x_center},{self.y_center})")
+    
+    def __gt__(self, other: float) -> bool:
+        """Returns True if area of first shape is grater than the second shape"""
+        return self.area > other.area
+    
+    def __ge__(self, other: float) -> bool:
+        """Returns True if area of first shape is grater than or equal to the second shape"""
+        return self.area >= other.area
+    
+    def __lt__(self, other: float) -> bool:
+        """Returns True if area of first shape is smaler than the second shape"""
+        return self.area < other.area
+    
+    def __le__(self, other: float) -> bool:
+        """Returns True if area of first shape is smaller than or equal to the second shape"""
+        return self.area <= other.area
+    
+    def __eq__(self, other: float) -> bool:
+        """Returns True if shapes are of the same type and have the same area"""
+        if self.area == other.area and type(self) == type(other):
+            return True
+        else:
+            return False
+
+    def translate(self, x_new: float|int, y_new: float|int) -> GeomShape:
+        """Moves shape to new center point. Origial shape is unchanged"""
+        new_shape = copy(self)
+        if not isinstance(x_new, (float, int)):
+            raise TypeError (f"x_new must be int or float and not {type(x_new)}")
+        if not isinstance(y_new, (float, int)):
+            raise TypeError (f"y_new must be int or float and not {type(y_new)}")
+        new_shape.x_center = x_new
+        new_shape.y_center = y_new
+        return new_shape
+
+    #========================================================================================================================#
+
+import math
+
+class Circle(GeomShape):
+    """Circle with defined radius and thus calculated area and circumference"""
+    
+    def __init__(self, x_center: float|int, y_center: float|int, radius: float|int) -> None:
+        #print("C__init__ körs")
+        super().__init__(x_center, y_center)
+        self.radius = radius
+        self._area = None                               # To make sure value is None to begin with
+        self._circumference = None                      # To make sure value is None to begin with
+    
+    # getters
+    @property
+    def radius(self) -> float|int:
+        #print("C radus getter körs")
+        return self._radius
+    
+    @property
+    def area(self) -> float|int:
+        #print("C area getter körs")
+        if self._area is not None:                      # if values is assigend already, does not have to be calculated again
+            return self._area
+        else:
+            self._area = math.pi * self._radius ** 2    # måste vara ._area då finns ingen sätter; _radius does not go via getter
+            return self._area
+
+    @property
+    def circumference(self) -> float|int:
+        #print("C circumference getter körs")
+        if self._circumference is not None:
+            return self._circumference
+        else:
+            self._circumference = 2 * math.pi * self._radius
+            return self._circumference
+    
+    #setters
+    @radius.setter
+    def radius(self, value) -> float|int:
+        #print("C radius setter körs")
+        if not isinstance (value, (float, int)):
+            raise TypeError (f"Radius must be a float or an int and not {type(value)}")
+        if value <= 0:
+            raise ValueError ("Radius must be larger than 0")
+        self._radius = value
+    
+    # no setters for area or circumference as they are read-only
+
+    def __repr__(self) -> str:
+        return f"Circle ({self.x_center}, {self.y_center}, {self.radius})"
+    
+    def __str__(self) -> str:
+        return f"Circle: center (x,y) = ({self.x_center},{self.y_center}), radius = {self.radius}, area = {self.area}, circumference = {self.circumference}"
+    
+    def is_inside(self, x_point: float|int, y_point: float|int) -> bool:
+        """Checks if a given point is within the circumference of the shape"""
+        if not isinstance(x_point, (float, int)):
+            raise TypeError (f"x_point must be int or float and not {type(x_point)}")
+        if not isinstance(y_point, (float, int)):
+            raise TypeError (f"y_point must be int or float and not {type(y_point)}")
+
+        point_distance = math.sqrt((x_point-self.x_center)**2 + (y_point-self.y_center)**2)
+        if point_distance <= self.radius:
+            return True
+        else:
+            return False
+    
+    def is_unitcircle(self) -> bool:
+        """Checks if a circle is a unitcircle (radius = 1 and centerded at origo)"""
+        if self.radius == 1 and self.x_center == 0 and self.y_center == 0:
+            return True
+        else:
+            return False
+
+# =================================================================================================================#
+
+class Rectangle(GeomShape):
+    """Rectangle with defined sides (side a along x-axis, side b along y-axis) and thus calculated area and circumference"""
+    
+    def __init__(self, x_center: float|int, y_center: float|int, side_a: float|int, side_b: float|int) -> None:
+        #print("R__init__ körs")
+        super().__init__(x_center, y_center)                #ingen self med här då super() lägger på self själv
+        self.side_a = side_a
+        self.side_b = side_b
+        self._area = None
+        self._circumference = None
+    
+    # getters
+    @property
+    def side_a(self) -> float|int:
+        #print("R side_a getter körs")
+        return self._side_a
+    
+    @property
+    def side_b(self) -> float|int:
+        #print("R side_b getter körs")
+        return self._side_b
+    
+    @property
+    def area(self) -> float|int:
+        #print("R area getter körs")
+        if self._area is not None:
+            return self._area
+        else:
+            self._area = self.side_a * self.side_b
+            return self._area
+    
+    @property
+    def circumference(self) -> float|int:
+        #print("R circumference getter körs")
+        if self._circumference is not None:
+            return self._circumference
+        else:
+            self._circumference = 2 * (self.side_a + self.side_b)
+            return self._circumference
+    
+    #setters
+    @side_a.setter
+    def side_a(self, value) -> float|int:
+        #print("R side_a setter körs")
+        if not isinstance (value, (float, int)):
+            raise TypeError (f"Side must be a float or an int and not {type(value)}")
+        if value <= 0:
+            raise ValueError ("Side must be larger than 0")
+        self._side_a = value
+    
+    @side_b.setter
+    def side_b(self, value) -> float|int:
+        #print("R side_b setter körs")
+        if not isinstance (value, (float, int)):
+            raise TypeError (f"Side must be a float or an int and not {type(value)}")
+        if value <= 0:
+            raise ValueError ("Side must be larger than 0")
+        self._side_b = value
+    
+    # no setters for area or circumference as they are read-only
+
+    def __repr__(self) -> str:
+        return f"Rectancgle ({self.x_center}, {self.y_center}, {self.side_a}, {self.side_b})"
+    
+    def __str__(self) -> str:
+        return f"Rectangle: center (x,y) = ({self.x_center},{self.y_center}), sides = ({self.side_a}, {self.side_b}), area = {self.area}, circumference = {self.circumference}"
+    
+    def __eq__(self, other: float) -> bool:
+        """Returns True if shapes are of the same type, have the same area and the same side lenghts"""
+        eq_GeomFig_result = super().__eq__(other)
+        if eq_GeomFig_result and self.side_a == other.side_a:
+            return True
+        else:
+            return False
+    
+    def is_inside(self, x_point: float|int, y_point: float|int) -> bool:
+        """Checks if a given point is within the circumference of the shape"""
+        if not isinstance(x_point, (float, int)):
+            raise TypeError (f"x_point must be int or float and not {type(x_point)}")
+        if not isinstance(y_point, (float, int)):
+            raise TypeError (f"y_point must be int or float and not {type(y_point)}")
+
+        x_max = self.x_center + self.side_a/2
+        x_min = self.x_center - self.side_a/2
+        y_max = self.y_center + self.side_b/2
+        y_min = self.y_center - self.side_b/2
+
+        if x_point <= x_max and x_point >= x_min and y_point <= y_max and y_point >= y_min:
+            return True
+        else:
+            return False
+    
+    def is_square(self) -> bool:
+        """Checks if the rectangle is a square"""
+        if self.side_a == self.side_b:
+            return True
+        else:
+            return False
--- Labb-3/Shape/geometry.py
+++ Labb-3/Shape/geometry.py
@@ -12,19 +12,19 @@ class Shapes:
 
 
     # String represantation
-    def __repr__(self) -> None:
-        return f"{self.x_pos}{self.y_pos}"
+    def __repr__(self) -> str:
+        return f"Shape with x_pos = {self.x_pos}, y_pos ={self.y_pos}"
     
-    def __str__(self) -> None:
-        return f"X-pos is: {self.x_pos} and Y-pos is: {self.y_pos}"
+    def __str__(self) -> str:
+        return f"Shape with x_pos = {self.x_pos}, y_pos ={self.y_pos}"
 
     #----- Getter and Setter for x_pos
     @property
-    def x_pos(self):
+    def x_pos(self) ->(int | float):
         return self._x_pos
 
     @x_pos.setter
-    def x_pos(self, value: int | float):
+    def x_pos(self, value: (int | float)):
         #must be a number:
         print("x_pos.setter")
         if not isinstance(value, (int, float)):
@@ -36,12 +36,12 @@ class Shapes:
 
     #------- Getter and setter for y_pos
     @property
-    def y_pos(self):
+    def y_pos(self) -> (int | float):
     #Read and write for y_pos
         return self._y_pos
 
     @y_pos.setter
-    def y_pos(self, value: int | float):
+    def y_pos(self, value: (int | float)):
         print("y_pos.setter")
         if not isinstance(value, int, float):
             raise TypeError(f"y_pos input must be a number, not {type(value)}")
@@ -49,7 +49,7 @@ class Shapes:
         #Round to 1 decimal
         self._y_pos = round(value, 1)
 
-    #---- Overload ----
+    #---- Overload --------------------------------------------------
 
     def __lt__(self, other: Shapes) -> bool:
         """Checks lesser than operator for all shapes"""
@@ -71,10 +71,10 @@ class Shapes:
         """Checks if its equal to all shapes"""
         return self.area == other.area
 
-    #--------- #KUDOS TO DOROTA FOR THIS LINE OF CODE----------
-    def translatemethod(self, x_other: int|float, y_other: int|float) -> None:
+    #--------- #Translate method ----------
+    def translatemethod(self, x_other: int|float, y_other: int|float) -> Shapes:
         """Moves shape from the original position to the new position"""
-        new_shape = copy(self)
+        new_shape = copy(self)#KUDOS TO DOROTA FOR THIS LINE OF CODE
         if not isinstance(x_other, (int, float)):
             raise TypeError(f"Number has to be an int or float{type(x_other)}")
         if not isinstance(y_other, (int, float)):
@@ -83,161 +83,153 @@ class Shapes:
         self.y_center = y_other
         return self
 
-
-    #-------------------------------------------------------------------------------
-
-"""Inherets from geometry(Shapes) class"""
+#--------------------------
+"""Inherits from geometry(Shapes) class"""
 class Circle(Shapes):
-    def __init__(self, x_pos: float, y_pos: float, radius: float):
+    def __init__(self, x_pos: int|float, y_pos: int|float, radius: int|float) -> None:
         super().__init__(x_pos, y_pos)
         self.radius = radius
         # No value in area and circum from start
         self._circum = 0
         self._area = 0 
-        
-    
+
+
     #--- Property and decorator for radius, circum and area functions-----------------------
+    ## Getter for only radius because the rest is already fixat i shape
     @property
-    def radius(self) -> int | float:
+    def radius(self) -> (int | float):
         """Radius of circle"""
         return self._radius
 
     @radius.setter
-    def radius(self, value) -> int | float:
+    def radius(self, value: (int | float)):
         if not isinstance(value, (int | float)):
             raise TypeError(f"Number has to be an int or float not {type(value)}")
         if value <= 0:
             raise ValueError(f"Radius must be more than 0")
-        self._radius = value 
-
-
-    @property
-    def radius(self) -> int:
-        return self._radius
-
-    @radius.setter
-    def radius(self, value: (int | float)):
-        if not isinstance(value, (int, float)):
-            raise TypeError(f"Number must be an int or float, not {type(value)}")
-        if not (0 <= value <= 10):
-            raise ValueError("Number must be between 0 and 10")
-        self._radius = value 
-
+        self._radius = value
 
     @property
     def circum(self) -> (int | float):
-        return self._circum
-
-    @circum.setter
-    def circum(self) -> (int | float):
-        """Circumference of the circle"""
-        if self._circum is not None:
-            return self._circum
-        else:
-            self._circum = 2 * pi * self._radius
-            return self._circum
+        """Circum = Circumference"""
+        return 2 * pi * self.radius
 
     @property
     def area(self) -> (int | float):
         """Area of the circle"""
-        if self._area is not None:
-            return self._area
-        else:
-            self._area = pi * self._radius ** 2
+        return pi * self.radius**2
 
     # ------- Methods-------------------------------------------
 
 
+
+
     # String conversion
     def __repr__(self) -> None:
-        return f"{self.x_pos}{self.y_pos}{self.radius}"
+        return f"Circle with x_pos = {self.x_pos}, Circle with y_pos = {self.y_pos}, radius is = {self.radius}"
     
     def __str__(self) -> None:
-        return f"Circle, x position:{self.x_pos} Circle, y position: {self.y_pos}, radius is: {self.radius}, circumferene is: {self.circum}"
+        return f"Circle with x_pos = {self.x_pos} Circle with y_pos = {self.y_pos}, radius is = {self.radius}, circumference is = {self.circum}"
 
 
-    # --------------------------------------------------------
+    # --------------- Other methods -----------------------------------------
 
-    def is_it_enhetscirkel(self) -> bool:
-        """ Kollar om cirkeln är en enhetscirkel med radie1 vid origo"""
+    def is_enhetscirkel(self) -> bool:
+        """ Kollar om cirkeln är en enhetscirkel med radie 1 vid origo"""
         if self.radius == 1 and self.x == 0 and self.y == 0:
             return True
         else:
             return False
 
     
+
     ######## KUDOS to Daniel from this code solution calculating distance between two points####################
-    def is_inside_cirle(self, x, y):
+    def is_inside_circle(self, x, y) -> bool:
         """ Kollar om en viss punkt punkt befinner sig innanför objektet """
         return (x - self.x_pos)**2 + (y - self.y_pos)**2 < self.radius**2
 
-
+##_________________________________________________________________________________________________________________
 ## _______________________________________REKTANGEL CLASS__________________________________________________________
 class Rektangel(Shapes):
-    def __init__(self, x_pos, y_pos, height: int | float, width: int | float):
+    def __init__(self, x_pos: int | float, y_pos: int | float, height: int | float, width: int | float) -> list:
         super().__init__(x_pos, y_pos)
         self.height = height
         self.width = width
-
-
-    def __str__(self):
-        """Override for string"""
-        return "This is Rektangel x position: " + str(self.x_pos) + "and y postion of " + str(self.y_pos) + ". Height is " + str(self.height) + " and width is " + str(self.width)
-
-    ## Titta till detta sedan
-    def __repr__(self) -> (str):
-        return "Rektangels position x = " + str(self.x_pos) + ", position y = " + str(self.y_pos) + ", width = " + str(self.width) + ", height =" + str(self.height) + ")"
-
-
-
-    #------------- Propertys and decorators ----------------------------------
-    
+        self._area = 0
+        self._circum = 0
+        #------------------- Property and decorators ------------------------
     @property
     def height(self) -> (int | float):
-        """For height"""
+        """Height rektangel"""
         return self._height
 
     @height.setter
-    def height(self, value: int | float):
-        if isinstance(value, str):
-            raise TypeError(f"y pos must be an int or float ok? Not {type(value)}")
+    def height(self, value: (int | float)):
+        if not isinstance(value, (int, float)):
+            raise TypeError(f"Height must be an int or float ok? Not {type(value)}")
+        if value <= 0:
+            raise ValueError("Height must be larger than zero")
         self._height = value
-    
+
     @property
     def width(self) -> (int | float):
+        """Width rektangel"""
         return self._width
-    ##### KIKA IN DEN HÄR IGEN!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    ### ISÅFALL HÄR DET FELAR SENARE OM DET ÄR FEL med tanke på (int | float)
     @width.setter
-    def width(self, value: int | float):
-        if isinstance(value, str):
-            raise TypeError(f"y position must be an int or float ok? Not {type(value)}")
-        self.width = value
+    def width(self, value: (int | float)):
+        if not isinstance(value):
+            raise TypeError(f"Width must be an int or float ok? Not {type(value)}")
+        if value <= 0:
+            raise ValueError("Width must be larger than zero")
+        self._width = value
 
-
-    # Logic for width * height = area
-    @ property
-    def area(self) -> float:
-        return self.height * self.width
-    
-    # circum = Circumference
     @property
-    def circum(self) -> float:
-        return (self.width * 2) + (self.height * 2)
+    def area(self) -> (int | float):
+        """Area rektangel"""
+        return self.width * self.height
     
     @property
-    def is_it_square(self) -> True:
+    def circum(self) -> (int | float):
+        """Circumference rektangel"""
+        return self.width * 2 + self.height * 2
+
+
+    #------------------------------------------------
+
+    ### KANSKE FLYTTA NER ÄNNU MER
+    # String conversion
+    def __str__(self) -> str:
+        """Self as string"""
+        return f"Rektangel center position: {self.x_pos}, {self.y_pos}, height:({self.height}) area:({self.area}) circumference:({self.circum})"
+
+    def __repr__(self) -> (str):
+        return "Rektangel x_pos:{self.x_pos}, y_pos:{self.x_pos}, height:({self.height} width:({self.width})"
+
+    ############### KAN BEHÖVA EN EQ HÄR
+
+###################################
+    def is_it_square(self) -> bool:
+        """Is rektangel a square?"""
         if self._width == self._height:
-            return print("Yes, it is a square")
-        else:
-            return print("No, this is not a square")
-    
-    def is_it_inside(self, x_pos, y_pos):
-        x_min = self.x_pos - (self.width / 2)
-        x_max = self.x_pos + (self.width / 2)
-        y_min = self.y_pos - (self.height / 2)
-        y_max = self.y_pos + (self.height /2)
-
-        if x_min < x_pos <x_max and y_min < y_pos < y_max:
             return True
-        return False
+        else:
+            return False
+    
+    def is_it_inside(self, x_other: (int | float), y_other: (int | float)) -> bool:
+        """Checks if rectangle contains a given point"""
+        if not isinstance(x_other, (int, float)):
+            raise TypeError(f"Must be an int or float and not {type(x_other)}")
+        if not isinstance(y_other, (int, float)):
+            raise TypeError(f"Must be an int or float and not{type(y_other)}")
 
+        x_min = self.x_other - (self.width / 2)
+        x_max = self.x_other + (self.width / 2)
+        y_min = self.y_other - (self.height / 2)
+        y_max = self.y_other + (self.height /2)
+
+        if x_other <= x_max and x_point >= x_min and y_other <= y_max and y_other >= y_min:
+            return True
+        else:
+            return False
