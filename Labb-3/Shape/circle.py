97d75a1a31095ff6b6fbbe3fe6bf6a2268e3b75a
JoelOscarsson <j_oscarsson@hotmail.com>
2022-10-12 17:13:56 +0200

    Updated

--- Labb-3/Shape/circle.py
+++
@@ -1,4 +0,0 @@
-#from __future__ import annotations
-# Imports from geometry.py and and than then Shapes module
-#from geometry import Shapes
-#from math import pi
--- Labb-3/Shape/rektangel.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-from geometry import Shapes # Imports from Shapes
-from circle import Circle
-
-
